897873c2ae2eba11398105dbbb83a636a56b4c10
Florian Hauber <f.hauber@students.htl-leonding.ac.at>
2026-06-02 15:07:25 +0200

    Auto-commit: 2026-06-02 15:07:25

--- PeopleCsvDatei/People.cs
+++ PeopleCsvDatei/People.cs
@@ -1,10 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace PeopleCsvDatei
+﻿namespace People
 {
     internal class People
     {
+        private string _lastName = "";
+        private string _firstName = "";
+        private int _yearOfBirth = 0;
+
+        public string GetLastName()
+        {
+            return _lastName;
+        }
+
+        public string GetFirstName()
+        {
+            return _firstName;
+        }
+
+        public int GetYearOfBirth()
+        {
+            return _yearOfBirth;
+        }
+
+        public void SetLastName(string lastName)
+        {
+            _lastName = lastName;
+        }
+
+        public void SetFirstName(string firstName)
+        {
+            _firstName = firstName;
+        }
+
+        public void SetYearOfBirth(int yearOfBirth)
+        {
+            _yearOfBirth = yearOfBirth;
+        }
     }
 }
